be67e17506d1ba83cdb4af9e424ab84c96e101a8
bastien dubray <bastien.dubray@gmail.com>
2024-02-20 18:02:32 +0100

    Front + read

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do |article|
+  article = Article.new(title: Faker::Artist.name, content: Faker::Quote.matz)
+  article.save
+end
